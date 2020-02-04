f815b245b5740eabef6ffbecf8f5c409f872c278
Brandon Weaver <a7xncob@gmail.com>
2020-02-04 21:53:57 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
 class SessionsController < ApplicationController
   before_action :verify_logged_in
   
-  def login
+  def new
+  end
+  
+  def create
+    session[:name] = params[:name]
+    redirect_to secret_path
+  end
+  
+  def destroy
+    session.delete :name
   end
   
 end
