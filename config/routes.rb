49dc64a3c0343d5b9fff1d9bf9a95947b5966b23
Brandon Weaver <a7xncob@gmail.com>
2020-02-04 21:51:02 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   get "/login" => "sessions#new"
-  post "/login" => ""
+  post "/login" => "sessions#create"
+  
+  get "/logout" => "sessions#destroy"
+  
   get "/secret" => "secrets#show"
+  
 end
